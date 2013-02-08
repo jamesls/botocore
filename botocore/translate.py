5467d6a7244c181a01212cbb9f1296747586db2c
James Saryerwinnie <js@jamesls.com>
2013-02-08 11:55:49 -0800

    Initial commit of model building
    This gives us a chance to annotate the models in whatever way
    we need.  I've added support for:
    
    * Adding new top level keys.
    * Updating anything in the 'operations' dict with new attributes
      (param annotations, operation annotations, etc).
    * Merging in the data from _services.json into the 'metadata' top
      level key.
    
    Things I've left out for the time being:
    
    * Annotating snake case conversions for ops/params.
    * Adding python types to the model types.

---
+++ botocore/translate.py
@@ -0,0 +1,110 @@
+"""Translate the raw json files into python specific descriptions."""
+import os
+import sys
+import json
+from copy import deepcopy
+try:
+    from collections import OrderedDict
+except ImportError:
+    # OrderedDicts are used during the annotation process to preserve
+    # the order of keys to create useful diffs.  Not doing this doesn't
+    # break the behavior, it just creates less the useful diffs.
+    # This only applies for python2.6, so we could include a backport
+    # of ordered dict for python2.6 if we felt this was important.
+    OrderedDict = dict
+
+
+class ModelFiles(object):
+    """Container object to hold all the various parsed json files.
+
+    Includes:
+
+        * The json service description.
+        * The _services.json file.
+        * The _regions.json file.
+        * The <service>.py enhancements file.
+        * The name of the service.
+
+    """
+    def __init__(self, model, services, regions, enhancements, name=''):
+        self.model = model
+        self.services = services
+        self.regions = regions
+        self.enhancements = enhancements
+        self.name = name
+
+
+def load_model_files(args):
+    model = json.load(open(args.modelfile),
+                           object_pairs_hook=OrderedDict)
+    services = json.load(open(args.services_file),
+                        object_pairs_hook=OrderedDict)
+    regions = json.load(open(args.regions_file),
+                        object_pairs_hook=OrderedDict)
+    enhancements = json.load(open(args.regions_file),
+                             object_pairs_hook=OrderedDict)
+    service_name = os.path.splitext(os.path.basename(args.modelfile))[0]
+    return ModelFiles(model, services, regions, enhancements,
+                      name=service_name)
+
+
+def _import_python_file(file_path):
+    directory = os.path.dirname(os.path.abspath(file_path))
+    sys.path.append(directory)
+    module_name = os.path.splitext(os.path.basename(file_path))[0]
+    try:
+        module = __import__(module_name)
+        enhancements = getattr(module, 'MODEL', {})
+    except ImportError:
+        # This file is optional, so if it doesn't exist, an empty
+        # dict is returned.
+        enhancements = {}
+    return enhancements
+
+
+def translate(model):
+    new_model = deepcopy(model.model)
+    transform_operations_list(new_model)
+    new_model.update(model.enhancements.get('extra', {}))
+    service_info = model.services.get(model.name, {})
+    merge_pagination_configs(
+        new_model,
+        model.enhancements.get('extra', {}).get('pagination', {}))
+    new_model['metadata'] = service_info.copy()
+    new_model['operations'].update(model.enhancements.get('operations', {}))
+    return new_model
+
+
+def merge_pagination_configs(new_model, pagination):
+    for name in pagination:
+        config = pagination[name]
+        operation = new_model['operations'].get(name)
+        if operation is None:
+            raise ValueError("Tried to add a pagination config for non "
+                             "existent operation '%s'" % name)
+        operation['pagination'] = config.copy()
+
+
+def transform_operations_list(new_model):
+    """Transforms list of operations into a dict.
+
+    This mutates the passed in new_model.
+
+    From:
+
+        {'operations': [{'Name': 'Foo'}, {'Name': 'Bar'}]}
+
+    To:
+
+        {'operations': {'Foo': {}, 'Bar': {}}}
+
+    The 'Name' key is extracted out of the per operation dict,
+    but all the other fields are left in tact.
+
+    """
+    operations = new_model.get('operations', [])
+    operations_map = OrderedDict()
+    for operation in operations:
+        name = operation.pop('name')
+        operations_map[name] = operation
+    new_model['operations'] = operations_map
