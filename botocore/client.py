4fdfb37ca7cb4713340a13601be52e7a4c608559
James Saryerwinnie <js@jamesls.com>
2015-02-26 15:49:38 -0800

    Break out serialization into a separate client method

--- botocore/client.py
+++ botocore/client.py
@@ -224,6 +224,29 @@ class BaseClient(object):
 
     def _make_api_call(self, operation_name, api_params):
         operation_model = self._service_model.operation_model(operation_name)
+        request_dict = self._convert_to_request_dict(
+            api_params, operation_model)
+
+        http, parsed_response = self._endpoint.make_request(
+            operation_model, request_dict)
+
+        self.meta.events.emit(
+            'after-call.{endpoint_prefix}.{operation_name}'.format(
+                endpoint_prefix=self._service_model.endpoint_prefix,
+                operation_name=operation_name),
+            http_response=http, parsed=parsed_response,
+            model=operation_model
+        )
+
+        if http.status_code >= 300:
+            raise ClientError(parsed_response, operation_name)
+        else:
+            return parsed_response
+
+    def _convert_to_request_dict(self, api_params, operation_model):
+        # Given the API params provided by the user and the operation_model
+        # we can serialize the request to a request_dict.
+        operation_name = operation_model.name
         event_name = (
             'before-parameter-build.{endpoint_prefix}.{operation_name}')
         self.meta.events.emit(
@@ -242,22 +265,7 @@ class BaseClient(object):
             model=operation_model, params=request_dict,
             request_signer=self._request_signer
         )
-
-        http, parsed_response = self._endpoint.make_request(
-            operation_model, request_dict)
-
-        self.meta.events.emit(
-            'after-call.{endpoint_prefix}.{operation_name}'.format(
-                endpoint_prefix=self._service_model.endpoint_prefix,
-                operation_name=operation_name),
-            http_response=http, parsed=parsed_response,
-            model=operation_model
-        )
-
-        if http.status_code >= 300:
-            raise ClientError(parsed_response, operation_name)
-        else:
-            return parsed_response
+        return request_dict
 
     def _sign_request(self, operation_name=None, request=None, **kwargs):
         # Sign the request. This fires its own events and will
